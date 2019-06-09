f08aed2b670bb0cb1f2938978d5db30e81d50693
Jefta Dirksen <jeftadirksen@gmail.com>
2019-06-09 12:31:44 +0200

    Add project files.

---
+++ CfW.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.IO;
+
+namespace CfW
+{
+    class CfW
+    {
+        public string Filename;
+
+        public CfW(string filename)
+        {
+            Filename = filename;
+        }
+
+        public void Run()
+        {
+            int lastCheckMin = DateTime.Now.Minute;
+            while (true)
+            {
+                if (DateTime.Now.Minute != lastCheckMin)
+                {
+                    lastCheckMin = DateTime.Now.Minute;
+                    Console.WriteLine(lastCheckMin);
+                    CheckForMatch(DateTime.Now);
+                }
+                Thread.Sleep(1000);
+            }
+        }
+
+        public void CheckForMatch(DateTime dt)
+        {
+            using(StreamReader file = new StreamReader(Filename))
+            {
+                string line;
+                while (!file.EndOfStream)
+                {
+                    line = file.ReadLine();
+                    Console.WriteLine(String.Format("{0} {1}", dt.ToString(), line));
+                }
+            }
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace CfW
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CfW cfw = new CfW("test.cfw");
+            //cfw.Run();
+            cfw.CheckForMatch(Convert.ToDateTime("2019-06-09 12:00"));
+
+            Console.WriteLine("Bye!");
+            Console.ReadKey();
+        }
+    }
+}
